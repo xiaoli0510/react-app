8a41ecd6b959a5ac02a57b68ee7faa5568287893
xiaoxiao <253451518@qq.com>
2021-01-13 12:50:09 +0800

    表单

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import './App.css'
 import React from 'react'
 
+class Reservation extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isGoing:true,
+      numbersOfGuest:2
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-class Page extends React.Component{
-  render(){
-    const numbers = this.props.numbers
-    const listItem = numbers.map(item=>{
-      return <li key={item.toString()}>{item}</li>
+  handleInputChange(event){
+    const name = event.target.name;
+    const value = name==='isGoing'?event.target.checked:event.target.value;
+    this.setState({
+      [name]:value
     })
-    return (
-      <ul>{listItem}</ul>
+  }
+
+  handleSubmit(event){
+    console.log('提交的参与是：'+this.state.isGoing);
+    console.log('提交的来宾人数是：'+this.state.numbersOfGuest);
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          参与：
+          <input type="checkbox" checked={this.state.isGoing} name="isGoing" onChange={this.handleInputChange}/>
+        </label>
+        <br/>
+        <label>
+          来宾人数：
+          <input type="number" name="numbersOfGuest" value={this.state.numbersOfGuest} onChange={this.handleInputChange}/>
+        </label>
+        <br/>
+        <input type="submit" value="提交" />
+      </form>
     )
   }
 }
 
 
+
+
 function App(){
-  const numbers = [1,2]
   return (
     <div className="App-header">
-    <Page numbers={numbers}/>
+    <Reservation/>
     </div>
   )
 }
