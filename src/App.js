94aa817e926ad8f0c0603866acab34a8fc6d34ef
xiaoxiao <253451518@qq.com>
2021-04-20 19:00:42 +0800

    不使用es6

--- src/App.js
+++ src/App.js
@@ -1,47 +1,56 @@
 import './App.css'
 import React from 'react'
+var createReactClass = require('create-react-class');
 
+//使用es6和使用create-react-class模块的区别
 /*
-在jsx中使用点语法
-自定义组件需要大写开头，不然react会把这个元素当做HTML标签。如果自定义的是小写开头的，在使用之前，需将自定义组件名赋值给一个大写开头的变量名。
-jsx不能是一个表达式，需通过将表达式赋值给大写开头的变量来解决此问题。
-js表达式作为props。
-if for不是表达式，不能直接在jsx中使用。
-可直接将字符串字面量赋值给prop。
-props默认值为true。
-props属性展开。
-jsx可以返回一组数组，需注明key。
-js表达式可作为子元素。
-函数作为子元素
+(1)声明默认属性：es6中使用 class组件名.defaultProps来定义默认的props属性。create-react-class需要定义getDefaultProps函数来返回默认的props属性。
+(2)初始化state。es6中直接在constructor函数里面初始化state。create-react-class需要定义getInitialState函数来返回默认的state属性。
+(3)自动绑定。es6中事件需在constructor里面进行 事件名.bind(this)。create-react-class中自动绑定this。
+(4)mixins。es6中不支持mixins。create-react-class中支持mixins；如果有多个mixins,mixins会按照定义时的顺序执行,且会调用每一个mixins中的生命周期方法。
 */
-
-//将函数push到items，注意,props.children是一个函数，需要执行props.children(i)才返回div
-function Repeat(props){
-  let items = [];
-  for(let i= 0;i<props.numTimes;i++){
-    items.push(props.children(i));
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: 1
+    }
   }
-  return <div>{items}</div>;
+  render() {
+    return (
+      <div > name:{this.props.name};age:{this.state.age} </div>
+    )
+  }
 }
 
-//函数作为子元素
-function ListOfTenThings(){
-  const numTimes = 10;
-  return (
-    <Repeat numTimes={numTimes}>
-      {(index)=><div key={index}>This is item {index} in the list。</div>}
-    </Repeat>
-  )
+Greeting.defaultProps = {
+  name: 'hary'
 }
 
+var Greeting1 = createReactClass({
+  getInitialState:function(){
+    return {
+      age:2
+    }
+  },
+  getDefaultProps: function () {
+    return {
+      name: 'Alice'
+    }
+  },
+  render: function () {
+    return (
+      <div > name:{this.props.name};age:{this.state.age} </div>
+    )
+  }
+})
 
 
 function App(props) {
-  return ( 
-    <>
-   <ListOfTenThings/>
-    
-    </>
+  return ( <div>
+    <Greeting/ >
+    <Greeting1/ >
+    </div>
   )
 }
 
