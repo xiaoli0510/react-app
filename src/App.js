99d6f0e1216e2d04cc36ec4773b2d9b41e702744
xiaoxiao <253451518@qq.com>
2021-04-21 19:08:00 +0800

    Refs & DOM

--- src/App.js
+++ src/App.js
@@ -1,30 +1,76 @@
 import './App.css'
 import React from 'react'
-import { readdirSync } from 'fs';
-var createReactClass = require('create-react-class');
 
-//不使用jsx
-//每一个jsx实际上是使用React.create(dom,props,children)的语法糖
-class Greeting extends React.Component{
+/*
+Refs&Dom:
+使用ref去存储DOM节点的引用。React会在组件挂载的时候给current属性传入DOM元素，并在组件卸载的时候传入null值。ref会在componentDidMount()和componentDidUpdate()生命周期钩子触发前更新。
+默认情况下，不能在函数组件上使用ref属性，因为函数组件没有实例。在函数组件内部使用ref属性时，需使用useRef(null)来生成ref,并将生成的ref指向一个DOM元素或class组件。
+*/
+
+class CustomTextInput extends React.Component{
+  constructor(props){
+    super(props);
+    this.textInput = React.createRef();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    this.textInput.current.focus();
+    // this.$refs.textInput.current.focus();
+  }
   render(){
-    return <div>Hello {this.props.toWhat}</div>
+    return (
+      <div>
+        <input type="text" ref={this.textInput}/>
+        <input type="button" value="Focus the text input" onClick={this.handleClick}/>
+      </div>
+    )
   }
 }
 
-//将上面的jsx编写为不使用jsx的代码
-class App  extends React.Component{
+//在父组件中使用ref来获取这个自定义的input组件并手动调用他的handleClick方法
+class AutoFocusTextInput extends React.Component{
+  constructor(props){
+    super(props);
+    this.textInput = React.createRef();
+  }
+  componentDidMount(){
+    this.textInput.current.handleClick();
+  }
   render(){
-    return React.createElement('div',null,`Hello ${this.props.toWhat}`)
+    return (
+      <CustomTextInput ref={this.textInput}/>
+    )
   }
 }
 
+//使用ref回调函数
+function CustomTextInput1(props){
+  return (
+    <div>
+     <input ref={props.inputRef}/>
+    </div>
+  )
+}
+  
+class Parent extends React.Component{
+  constructor(props){
+    super(props);
+    this.inputElement=null
+  }
+    render(){
+      return (
+        <CustomTextInput1 inputRef={el=>this.inputElement=el}/>
+        )
+    }
+}
 
-// function App(props) {
-//   return ( <div>
-//     <Greeting/ >
-//     </div>
-//   )
-// }
+function App(props) {
+  return ( <div>
+    <Parent/ >
+    </div>
+  )
+}
 
 
 export default App
+
